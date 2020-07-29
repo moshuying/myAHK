d1cd4a1d40def109a96c2b57fcaee8e3f6a69a00
刘九江 <1460083332@qq.com>
2020-07-29 20:13:16 +0800

    完善powershell.c

--- powershell.c
+++ powershell.c
@@ -1,6 +1,7 @@
-// gcc powershell.c -o powershell
+// window 10 default powershell.exe in C:\Windows\System32\WindowsPowerShell\v1.0
+// gcc powershell.c -o powershell.exe
 #include <stdlib.h>
 int main(){
-  system("start \"MoShuYing\" /D \"c:\\\" \"C:\\Program Files\\PowerShell\\7\\pwsh.exe\"");
+  system("start \"PowerShell 7 (x64)\" /D \"c:\\\" \"C:\\Program Files\\PowerShell\\7\\pwsh.exe\"");
   return 0;
 }
