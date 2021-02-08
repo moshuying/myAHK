ce1966cf1812c0a730c0bf78505546c3ea56b3e7
moshuying <1460083332@qq.com>
2021-02-08 20:22:21 +0800

    更新安装脚本

--- install.ps1
+++ install.ps1
@@ -13,13 +13,3 @@ cd myAHK;
 
 # 添加默认启动
 New-Item -ItemType SymbolicLink -Path "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Name ".\easyWork.ahk.lnk" -Value ".\easyWork.ahk"
-
-# 按步骤执行，点开看的你知道可以怎样操作
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
-choco install git autohotkey.install -y;
-git clone https://gitee.com/moshuying/myAHK.git;
-cd myAHK;
-.\easyWork.ahk;
-New-Item -ItemType SymbolicLink -Path "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Name ".\easyWork.ahk.lnk" -Value ".\easyWork.ahk"
